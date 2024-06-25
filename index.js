1efc30d5a063053f55cc576ec5cafa8994847fa6
Julie Rindone <julie.rindone@gmail.com>
2024-06-25 10:53:27 -0700

    Added  textArea, tweetInput and tweetBtn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 import { tweetsData } from "./data.js";
 
+const tweetInput = document.getElementById("tweet-input");
+const tweetBtn = document.getElementById("tweet-btn")
+
+tweetBtn.addEventListener('click', () => {
+    console.log(tweetInput.value);
+});

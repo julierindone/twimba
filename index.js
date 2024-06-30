23a601668b5b780b62e672ab31a1c7ceabc180a5
Julie Rindone <julie.rindone@gmail.com>
2024-06-30 08:03:37 -0500

    Added render(); Fixed return feedHtml location

--- index.js
+++ index.js
@@ -11,10 +11,7 @@ tweetBtn.addEventListener('click', () => {
 function getFeedHtml() {
   let feedHtml = ''
 
-  // for (let tweet of tweetsData) {
-  tweetsData.forEach(function(tweet) {
-    console.log(`handle: ${tweet.handle}`);
-
+  tweetsData.forEach(function(tweet){
     feedHtml +=
     `<div class="tweet">
       <div class="tweet-inner">
@@ -30,20 +27,12 @@ function getFeedHtml() {
         </div>
       </div>
     </div>`
-    feed.innerHTML = feedHtml
   })
-
-
-  /*
-Challenge:
-1. Use a "for of" to iterate over the data and 
-  create HTML string for each tweet using the 
-  boilerplate below. Replace UPPERCASE text
-  with data from the tweets. 
-2. Store this HTML in a let called "feedHtml".
-3. Log out feedHtml.
-4. Call getFeedHtml to check it's working.
-*/
+  return feedHtml
 }
 
-getFeedHtml(tweetsData)
+function render() {  
+    document.getElementById("feed").innerHTML = getFeedHtml()
+}
+
+render()

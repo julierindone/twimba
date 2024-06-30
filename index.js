d505917880de2b061972d7383d9e1fba1e874106
Julie Rindone <julie.rindone@gmail.com>
2024-06-30 08:04:24 -0500

    Added icons

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import { tweetsData } from "./data.js";
 
 const tweetInput = document.getElementById("tweet-input");
 const tweetBtn = document.getElementById("tweet-btn")
-const feed = document.getElementById("feed")
 
 tweetBtn.addEventListener('click', () => {
   console.log(tweetInput.value);
@@ -13,16 +12,25 @@ function getFeedHtml() {
 
   tweetsData.forEach(function(tweet){
     feedHtml +=
-    `<div class="tweet">
+      `<div class="tweet">
       <div class="tweet-inner">
         <img src="${tweet.profilePic}" class="profile-pic">
         <div>
           <p class="handle">${tweet.handle}</p>
           <p class="tweet-text">${tweet.tweetText}</p>
           <div class="tweet-details">
-            <span class="tweet-detail">${tweet.replies.length}</span>
-            <span class="tweet-detail">${tweet.likes}</span>
-            <span class="tweet-detail">${tweet.retweetslength}</span>
+            <span class="tweet-detail">
+              <i class="fa-regular fa-comment-dots"></i>
+              ${tweet.replies.length}
+            </span>
+            <span class="tweet-detail">
+              <i class="fa-solid fa-heart"></i>
+              ${tweet.likes}
+            </span>
+            <span class="tweet-detail">
+              <i class="fa-solid fa-retweet"></i>
+              ${tweet.retweets}
+            </span>
           </div>
         </div>
       </div>
